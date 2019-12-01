e1c0d3e1906a4476839e875c0583118f24b8bce9
ballen2024 <brendan.layne@gmail.com>
2019-11-30 22:10:34 -0800

    normalized abbrevations; built first model

--- DataScience/deliverable1.r
+++ DataScience/deliverable1.r
@@ -1,4 +1,4 @@
-## ----warning=FALSE-------------------------------------------------------
+## ----warning=FALSE------------------------------------------------------------------------------------------------------------------------------------------------------
 suppressMessages(library(tidyr))
 suppressMessages(library(dplyr))
 suppressMessages(library(tidyverse))
@@ -8,7 +8,8 @@ suppressMessages(library(lemon))
 suppressMessages(library(scales))
 knit_print.data.frame <- lemon_print
 
-## ----warning=FALSE-------------------------------------------------------
+
+## ----warning=FALSE------------------------------------------------------------------------------------------------------------------------------------------------------
 raw <- suppressMessages(read_csv("https://query.data.world/s/jo4zezsyrfp3rnutrsmjk7nzw75npd"))
 
 game_data <- tibble(date=raw$date, 
@@ -29,14 +30,17 @@ game_data <- tibble(date=raw$date,
 game_data <- game_data %>%
   na.omit()
 
-## ----render=lemon_print, echo=FALSE--------------------------------------
+
+## ----render=lemon_print, echo=FALSE-------------------------------------------------------------------------------------------------------------------------------------
 head(game_data)
 df <- summary(game_data[7:14])
 
-## ----render=lemon_print, echo=FALSE--------------------------------------
+
+## ----render=lemon_print, echo=FALSE-------------------------------------------------------------------------------------------------------------------------------------
 head(df)
 
-## ---- echo=FALSE, render=lemon_print-------------------------------------
+
+## ---- echo=FALSE, render=lemon_print------------------------------------------------------------------------------------------------------------------------------------
 t <- table(game_data$day_night)
 day_night_data <- as.data.frame(t)
 colnames(day_night_data) <- c("Game Time", "Number of Games")
@@ -44,7 +48,8 @@ day_night_data$`Game Time` <- c("Day","Night")
 head(day_night_data)
 # p <- ggplot(day_night_data, aes(x=Var1,y=Freq)) + geom_bar(stat = "identity", fill="steelblue3") + geom_text(aes(label= paste("Count = ", day_night_data$Freq)), vjust=1.6, color="white", size=4) + labs(x = "Game Time (Day/Night)",y="Number of Games",title="Frequency of Day/Night Games from 1871-2016")
 
-## ----echo = FALSE--------------------------------------------------------
+
+## ----echo = FALSE-------------------------------------------------------------------------------------------------------------------------------------------------------
 require(scales)
 date <- as.character(game_data$date)
 year <- substr(date, 1, 4)
